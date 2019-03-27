a6962ef434b00012257a6a8eda09bbc80463198e
Gurinder Singh <gurinder.singh@1e.com>
2019-03-27 15:32:42 +0530

    Did an initiali implementatin of the deploy command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+use structopt::StructOpt;
+use quicli::prelude::*;
+use hyperv_rs::Hyperv;
+
+#[derive(Debug, StructOpt)]
+enum Subcommand {
+    #[structopt(name = "deploy")]
+    Deploy {
+        path: PathBuf
+    },
+}
+
+fn main() -> CliResult {
+    let Subcommand::Deploy { path } = Subcommand::from_args();
+    Hyperv::import_vm(&path)?;
+    Ok(())
 }
