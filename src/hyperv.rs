bd973c85028859bef124d6765329d12ff1651cb5
Gurinder Singh <gurinder.singh@1e.com>
2019-04-01 13:52:49 +0530

    Got rid of compare_vm() method.
    We're unlikely to need it.

--- src/hyperv.rs
+++ src/hyperv.rs
@@ -4,7 +4,6 @@ use serde_derive::Deserialize;
 use uuid::Uuid;
 use std::fmt;
 use std::path::Path;
-use std::io::{BufReader, BufRead};
 use std::collections::HashMap;
 
 pub struct Hyperv;
@@ -73,60 +72,6 @@ impl Hyperv {
         Ok(vm)
     }
 
-    pub fn compare_vm<P: AsRef<Path>>(path: P, import_type: &ImportType) -> Result<Vec<VmIncompatibility>> {
-        let path = Self::validate_file_path(path.as_ref())?;
-        let command = format!(
-            "$report = compare-vm -Path \"{}\" {};
-            if ($?) {{ $report.Incompatibilities | Format-Table -Property MessageId, Message -HideTableHeaders }}",
-        path,
-        Self::generate_import_vm_param_stub(import_type));
-             
-        let output = Self::spawn_and_wait(&command)?;
-
-        Self::map_lines(output, |line: &str| {
-            let line = line.trim();
-            if line.is_empty() {
-                return Ok(None)
-            }
-            let mut parts = line.splitn(2, ' ');
-            let msg_id = parts.next().ok_or_else(|| HypervError { msg: "Failed to parse to VmIncomatibility. No MessageId in string".to_owned() })?;
-            let msg = parts.next().ok_or_else(|| HypervError { msg: "Failed to parse to VmIncomatibility. No Message in string".to_owned() })?;
-            let msg_id = msg_id.parse::<i64>().map_err(|e| HypervError { msg: format!("Failed to parse to VmIncomatibility. Cannot parse MessageId to i64: {}", e) })?;
-            Ok(Some(VmIncompatibility::from(msg_id, msg.to_owned())))
-        })
-    }
-
-    fn generate_import_vm_param_stub(import_type: &ImportType) -> String {
-        match import_type {
-            ImportType::RegisterInPlace => "".to_owned(),
-            ImportType::Restore { vhd_path, virtual_machine_path } => {
-                match (vhd_path, virtual_machine_path) {
-                    (None, None)  => "-Copy".to_owned(),
-                    (Some(vhdpath), None)  => format!("-Copy -VhdDestinationPath \"{}\"", vhdpath.to_string_lossy()),
-                    (None, Some(vmpath))  => format!("-Copy -VirtualMachinePath \"{}\"", vmpath.to_string_lossy()),
-                    (Some(vhdpath), Some(vmpath))  => format!("-Copy -VhdDestinationPath \"{}\" -VirtualMachinePath \"{}\"", vhdpath.to_string_lossy(), vmpath.to_string_lossy()),
-                }
-            },
-            ImportType::Copy { vhd_path, virtual_machine_path } => {
-                match (vhd_path, virtual_machine_path) {
-                    (None, None)  => "-GenerateNewId -Copy".to_owned(),
-                    (Some(vhdpath), None)  => format!("-GenerateNewId -Copy -VhdDestinationPath \"{}\"", vhdpath.to_string_lossy()),
-                    (None, Some(vmpath))  => format!("-GenerateNewId -Copy -VirtualMachinePath \"{}\"", vmpath.to_string_lossy()),
-                    (Some(vhdpath), Some(vmpath))  => format!("-GenerateNewId -Copy -VhdDestinationPath \"{}\" -VirtualMachinePath \"{}\"", vhdpath.to_string_lossy(), vmpath.to_string_lossy()),
-                }
-            }
-        }
-    }
-
-    fn validate_file_path(path: &Path) -> Result<&str> {
-        if !path.is_file() {
-            Err(HypervError::new("Path does not point to a valid file"))
-        } else {
-            let path = path.to_str().ok_or_else(|| HypervError { msg: "Bad path".to_owned() })?;
-            Ok(path)
-        }
-    }
-
     fn validate_dir_path(path: &Path) -> Result<&str> {
         if !path.is_dir() {
             Err(HypervError::new("Path does not point to a valid directory"))
@@ -136,21 +81,6 @@ impl Hyperv {
         }
     }
 
-    fn map_lines<T, F: Fn(&str) -> Result<Option<T>>>(stdout: Stdout, f: F) -> Result<Vec<T>> {
-        let mut vec = Vec::new();
-        for line in BufReader::new(stdout).lines() {
-            match line {
-                Ok(line) => {
-                    if let Some(t) = f(&line)? {
-                        vec.push(t)
-                    }
-                }
-                Err(e) => Err(HypervError::new(format!("Failed to process powershell output. Could not split stdout into lines: {}", e)))?,
-            }
-        }
-
-        Ok(vec)
-    }
 
     fn spawn(command: &str) -> Result<PsProcess> {
         PsCommand::new(command)
@@ -189,11 +119,6 @@ pub struct ImportedVm {
     pub missing_switches: HashMap<String, String>,
 
 }
-pub enum ImportType<'a, 'b> {
-    RegisterInPlace,
-    Restore { vhd_path: Option<&'a Path>, virtual_machine_path: Option<&'b Path> },
-    Copy { vhd_path: Option<&'a Path>, virtual_machine_path: Option<&'b Path> },
-}
 
 #[derive(Debug, Deserialize)]
 pub struct Vm {
@@ -205,55 +130,6 @@ pub struct Vm {
 
 // TODO: should this be a newtype?
 pub type VmId = Uuid;
-
-#[derive(Debug)]
-pub enum VmIncompatibility {
-    CannotCreateExternalConfigStore(String),
-    TooManyCores(String),
-    CannotChangeCheckpointLocation(String),
-    CannotChangeSmartPagingStore(String),
-    CannotRestoreSavedState(String),
-    MissingSwitch(String),
-    Other(String, i64),
-}
-
-impl VmIncompatibility {
-    fn from(msg_id: i64, msg: String) -> Self {
-        match msg_id {
-            13000 => VmIncompatibility::CannotCreateExternalConfigStore(msg),
-            14420 => VmIncompatibility::TooManyCores(msg),
-            16350 => VmIncompatibility::CannotChangeCheckpointLocation(msg),
-            16352 => VmIncompatibility::CannotChangeSmartPagingStore(msg),
-            25014 => VmIncompatibility::CannotRestoreSavedState(msg),
-            33012 => VmIncompatibility::MissingSwitch(msg),
-            msg_id => VmIncompatibility::Other(msg, msg_id)
-        }
-    }
-
-    pub fn message_id(&self) -> i64 {
-        match self {
-            VmIncompatibility::CannotCreateExternalConfigStore(_) => 13000,
-            VmIncompatibility::TooManyCores(_) => 14420,
-            VmIncompatibility::CannotChangeCheckpointLocation(_) => 16350,
-            VmIncompatibility::CannotChangeSmartPagingStore(_) => 16352,
-            VmIncompatibility::CannotRestoreSavedState(_) => 25014,
-            VmIncompatibility::MissingSwitch(_) => 33012,
-            VmIncompatibility::Other(_, i) => *i,
-        }
-    }
-
-    pub fn message(&self) -> &str {
-        match self {
-            VmIncompatibility::CannotCreateExternalConfigStore(s) => &s,
-            VmIncompatibility::TooManyCores(s) => &s,
-            VmIncompatibility::CannotChangeCheckpointLocation(s) => &s,
-            VmIncompatibility::CannotChangeSmartPagingStore(s) => &s,
-            VmIncompatibility::CannotRestoreSavedState(s) => &s,
-            VmIncompatibility::MissingSwitch(s) => &s,
-            VmIncompatibility::Other(s, _) => &s,
-        }
-    }
-}
 
 // TODO: We need to do proper design of error types. Just this one type is not enough
 #[derive(Debug, Fail)]
